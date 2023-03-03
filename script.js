01572bc825dc767cd7386d5d62cdd61d9e62366a
Ganeshdckap <ganeshmdckap@gmail.com>
2023-03-03 14:34:31 +0530

    My first push

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const submit = document.querySelector(".Submit")
 const results = document.querySelector(".result")
 const diff = document.querySelector(".diff")
 const total = document.querySelector(".total")
-console.log(total);
+// console.log(total);
 
 
 submit.addEventListener("click",()=>{
@@ -14,6 +14,7 @@ submit.addEventListener("click",()=>{
     let int1 = interest.value/1200
     let firstmethod= principal.value*int1 *((1+int1)**months.value)
     // console.log(firstmethod)
+    let years = months*12
     let secondmethod =((1+int1)**months.value-1)
     let emi = Math.round(Number(firstmethod/secondmethod))
     // console.log(result)
@@ -33,32 +34,3 @@ submit.addEventListener("click",()=>{
    
   }
 })
-
-
-
-
-
-
-
-
-
-
-
-// const p = document.getElementById("principal");
-// const r = document.getElementById("interest");
-// const n = document.getElementById("period");
-// const submit = document.querySelector("button");
-// const loan = document.querySelector("#loan-emi");
-// const differnce = document.querySelector("#difference")
-
-// submit.addEventListener("click",()=>{
-//     if (p.value && r.value && n.value !== "") {
-//         let r1 = r.value/1200
-//     let firstmethod = p.valuer1((1+r1)n.value)
-//     let secondmethod =((1+r1)n.value-1)
-//     let result = Math.floor(Number(firstmethod/secondmethod))
-//     loan.innerText = "₹"+result
-//     let differnce1 = result*n.value -p.value;
-//     differnce.innerText =differnce1 
-//     }
-// })
